271f9d3174fa1c3584dad6e5a02807ff143ef281
NikolaiIDS <nikolaisimeonov60@gmail.com>
2023-07-28 22:19:07 +0300

    Inventory
    Only single items for now

--- Backrooms/Assets/Scripts/Inventory/ItemHandler.cs
+++ Backrooms/Assets/Scripts/Inventory/ItemHandler.cs
@@ -1,42 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ItemHandler : MonoBehaviour
+using UnityEngine.UI;
+
+public class ItemHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    private bool isFollowingMouse = false;
-    private Vector3 offset;
+    public Image image; 
 
-    void Update()
+    [HideInInspector]
+    public Transform parentAfterDrag;
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        // Check if the mouse is being clicked
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Cast a ray from the camera to the mouse position
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+        Debug.Log("Begin drag");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
 
-            // Check if the ray hits the current game object
-            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
-            {
-                // Enable following the mouse
-                isFollowingMouse = true;
-                // Calculate the offset between the object's position and the mouse position
-                offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
-            }
-        }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        transform.position = Input.mousePosition;
+    }
 
-        // Check if the mouse button is released
-        if (Input.GetMouseButtonUp(0))
-        {
-            isFollowingMouse = false;
-        }
-
-        // If the object is following the mouse, update its position
-        if (isFollowingMouse)
-        {
-            Vector3 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)) + offset;
-            transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
-        }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End drag");
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
     }
 }
+
