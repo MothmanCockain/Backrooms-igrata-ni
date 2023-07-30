a87745117bcb142a4f105b8a8f58f78ee408c341
NikolaiIDS <nikolaisimeonov60@gmail.com>
2023-07-30 18:17:59 +0300

    fixed item
    fixed item to automatically find current player inventory ui

--- Backrooms/Assets/Scripts/Inventory/ItemHandler.cs
+++ Backrooms/Assets/Scripts/Inventory/ItemHandler.cs
@@ -2,21 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
 using UnityEngine.UI;
 
 public class ItemHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Image image;
-    public GameObject Inventory;
     [HideInInspector]
     public Transform parentAfterDrag;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Inventory.GetComponent<ItemHandler>();
+        //Inventory.GetComponent<ItemHandler>();
 
-
+        Transform Inventory = FindChildWithinCurrentRoot("Inventory");
         Debug.Log("Begin drag");
         parentAfterDrag = transform.parent;
         transform.SetParent(Inventory.transform);
@@ -35,6 +33,27 @@ public class ItemHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
         Debug.Log("End drag");
         transform.SetParent(parentAfterDrag);
         image.raycastTarget = true;
+    }
+
+    private Transform FindChildWithinCurrentRoot(string nameToFind)
+    {
+        Transform rootParent = transform.root;
+
+        // Loop through all the direct children of the root parent.
+        for (int i = 0; i < rootParent.childCount; i++)
+        {
+            Transform child = rootParent.GetChild(i);
+
+            // Check if the child's name matches the name we are searching for.
+            if (child.name == nameToFind)
+            {
+                // Return the Transform of the matching GameObject.
+                return child;
+            }
+        }
+
+        // Return null if the GameObject is not found within the current root.
+        return null;
     }
 }
 
